bb4972a72aaa4f986552982592653348512d7505
karinaskuy <dyah.karina125@gmail.com>
2025-06-08 03:26:12 +0700

    Update streamlit2.py

--- streamlit2.py
+++ streamlit2.py
@@ -3,6 +3,8 @@ import pandas as pd
 import plotly.express as px
 import datetime
 
+pip install -r requirements.txt
+
 # --- Konfigurasi Halaman ---
 st.set_page_config(
     page_title="Interactive Media Intelligence Dashboard",
